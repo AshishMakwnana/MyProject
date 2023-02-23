28fe587a43f2f79475a1e3e242bd5cb3b62c4db1
Ashish Makwana <ashishmakwana435@gmail.com>
2023-02-23 22:14:39 +0530

    Updates

---
+++ precache-manifest.5270cb9cb77ac6577dc1d269c14ccdde.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6fdfd98bff60d1a7ee4f",
+    "url": "/MyProject/static/css/main.54a655df.chunk.css"
+  },
+  {
+    "revision": "6fdfd98bff60d1a7ee4f",
+    "url": "/MyProject/static/js/main.6fdfd98b.chunk.js"
+  },
+  {
+    "revision": "fde221e0f7be23a8d12b",
+    "url": "/MyProject/static/js/1.fde221e0.chunk.js"
+  },
+  {
+    "revision": "29124111c9b6d37c1b77",
+    "url": "/MyProject/static/js/runtime~main.29124111.js"
+  },
+  {
+    "revision": "75581c404041c6b35351183141a6eaf1",
+    "url": "/MyProject/index.html"
+  }
+];
